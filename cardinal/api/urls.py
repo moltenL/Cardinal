62161860c295697aae1bca78f33ef622f437c9ee
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-16 00:10:15 -0700

    Add start to new views

--- cardinal/api/urls.py
+++ cardinal/api/urls.py
@@ -1,10 +1,11 @@
 # todo/api/urls.py : API urls.py
 from django.conf.urls import url
 from django.urls import path, include
-from .views import (
-    InitialApiView
-)
+from .views import InitialApiView, DataRequestApiView
 
 urlpatterns = [
-    path('test/', InitialApiView.as_view()),
+    path("test/", InitialApiView.as_view()),
+
+    # TODO: all, obj_tim, etc.
+    path("all/", DataRequestApiView.as_view()),
 ]
--- cardinal/api/views.py
+++ cardinal/api/views.py
@@ -12,3 +12,15 @@ class InitialApiView(APIView):
     def get(self, request, *args, **kwargs):
         """Return a cardinal"""
         return Response(CARDINAL_EMOJI, status=status.HTTP_200_OK)
+
+
+class DataRequestApiView(APIView):
+    # add permission to check if user is authenticated
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+
+        # TODO: pull from database
+        data = "foo"
+
+        return Response(data, status=status.HTTP_200_OK)
