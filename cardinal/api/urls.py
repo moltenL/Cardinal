fc1f3fdf3337261f8379d7f03e430d6cb907b73a
jakeroggenbuck <jakeroggenbuck2@gmail.com>
2021-09-25 13:21:17 -0700

    Add docs generator

--- cardinal/api/urls.py
+++ cardinal/api/urls.py
@@ -1,11 +1,28 @@
 from django.conf.urls import url
 from django.urls import path, include
-from .views import InitialApiView, DataRequestApiView, TestDataGeneratorApiView, api_docs
+from .views import InitialApiView, DataRequestApiView, TestDataGeneratorApiView
+from django.views.generic import TemplateView
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path("hello/", InitialApiView.as_view()),
-
     path("all/", DataRequestApiView.as_view()),
     path("generate/<str:data_structure_type>/", TestDataGeneratorApiView.as_view()),
-    path('', api_docs, name='index'),
+    url(
+        r'^openapi-schema',
+        get_schema_view(
+            title="Cardinal",
+            description="Cardinal",
+            version="1.0.0",
+            public=True,
+        ),
+        name='openapi-schema',
+    ),
+    url(
+        r'docs/',
+        TemplateView.as_view(
+            template_name='swagger-ui.html', extra_context={'schema_url': 'openapi-schema'}
+        ),
+        name='swagger-ui',
+    ),
 ]
