374c7954b7841b5a160102829f3bc14e013f4dad
Jake Roggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2021-10-02 11:25:31 -0700

    Add logger kwargs (#53)
    * Add logger kwargs
    
    * Line num

--- cardinal/api/logger.py
+++ cardinal/api/logger.py
@@ -3,7 +3,7 @@
 from datetime import datetime
 from os import path
 
-_FILE_PATH = 'request_log.txt'
+_FILE_PATH = "request_log.txt"
 
 """THE MAIN DECORATOR
 Please look up how decorators work, but in essence:
@@ -30,19 +30,25 @@ def request_logged(fn):
 # THE LOGGER
 # Takes in the arguments and the function and spits out some logging info
 def logger(cself, request, fn, *args, **kwargs):
-    log_file = open(_FILE_PATH, 'a')
+    log_file = open(_FILE_PATH, "a")
     # So (classname).get
     name = fn.__qualname__
     # (classname).get -> ['classname', 'get'] -> 'classname'
-    calling_class = name.split('.')[0]
+    calling_class = name.split(".")[0]
     args_string = (
         "\n".join(("\t" + str(arg)) for arg in args) + "\n" if len(args) != 0 else "[no arguments]"
+    )
+    kwargs_string = (
+        "\n".join(("\t" + f"{arg} : '{str(kwargs[arg])}'") for arg in kwargs.keys()) + "\n"
+        if len(kwargs.keys()) != 0
+        else "[no arguments]"
     )
     timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
     log_file.write(
         f"""Date: {timestamp}
 Request type: {calling_class}
 Arguments: {args_string}
+Keyword arguments: {kwargs_string}
 Query params:
     {request.query_params}
 
