f950fbfb429abbc4a6bdc65616d8bf858edbeef6
Adam Hutchings <55220104+adamhutchings@users.noreply.github.com>
2021-10-09 10:41:21 -0700

    Open DB on startup (#72)
    * Add dummy startup print
    
    * Open DB on cardinal startup
    
    * Don't reload (or start 2 processes)
    
    * Add pymongo req
    
    * Add dnspython
    
    * Black reformatting

--- cardinal/apps.py
+++ cardinal/apps.py
@@ -1,10 +1,13 @@
 from django.apps import AppConfig
+import pymongo
 
+from .api import cardinal_data_request as cdr
 
+# See ldc, database, clouddb from 'server'.
 class CardinalAPIConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cardinal'
 
     def ready(self):
-        # TODO - MongoDB stuff, log file opening, etc.
-        print('Apps: starting cardinal')
+        cdr.CLIENT = pymongo.MongoClient(cdr.CONNECTION_STR, cdr.PORT)
+        cdr.DB = cdr.CLIENT['2020caln']
--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export DJANGO_DEBUG=False
+export DJANGO_DEBUG=True
 export DJANGO_SECRET_KEY=$(cat ./scripts/key)
-python3 manage.py runserver 8001
+python3 manage.py runserver 8001 --noreload
