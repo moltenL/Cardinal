5030c46a40d467cac89b690fbca933437eebb4ee
Jake Roggenbuck <35516367+JakeRoggenbuck@users.noreply.github.com>
2021-10-14 20:10:12 -0700

    Testing key warning (#82)

--- scripts/testserver.sh
+++ scripts/testserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "WARNING! Cardinal is in testing mode using a key that is not safe for production"
 export DJANGO_DEBUG=True
 export DJANGO_SECRET_KEY=32498dsfdsklfj498wefksdlf894
 python3 manage.py runserver 8001 --noreload
